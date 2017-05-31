2d38fa4ead9f39626e19ba2b774c4e3a9353df93
Jacek Kunicki <jacek@kunicki.org>
2017-05-31 09:34:36 +0200

    Single line parser

--- src/main/scala/org/kunicki/reactive_streams_with_monix/importer/CsvImporter.scala
+++ src/main/scala/org/kunicki/reactive_streams_with_monix/importer/CsvImporter.scala
@@ -4,6 +4,8 @@ import java.nio.file.Paths
 
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
+import monix.eval.Task
+import org.kunicki.reactive_streams_with_monix.model.{InvalidReading, Reading, ValidReading}
 import org.kunicki.reactive_streams_with_monix.repository.ReadingRepository
 
 class CsvImporter(config: Config, readingRepository: ReadingRepository) extends LazyLogging {
@@ -13,4 +15,17 @@ class CsvImporter(config: Config, readingRepository: ReadingRepository) extends 
   private val concurrentFiles = config.getInt("importer.concurrent-files")
   private val concurrentWrites = config.getInt("importer.concurrent-writes")
   private val nonIOParallelism = config.getInt("importer.non-io-parallelism")
+
+  def parseLine(line: String): Task[Reading] = Task {
+    val fields = line.split(";")
+    val id = fields(0).toInt
+    try {
+      val value = fields(1).toDouble
+      ValidReading(id, value)
+    } catch {
+      case t: Throwable =>
+        logger.error(s"Unable to parse line $line: ${t.getMessage}")
+        InvalidReading(id)
+    }
+  }
 }
